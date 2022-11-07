87d814be3d0bd35d6483adc5163749e4319f359a
Rodrigo Wanzeler <wanzervai@gmail.com>
2022-11-07 18:08:59 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg';
+
+import './styles.css';
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="//www.instagram.com/wanzeler.r">wanzeler.r</a>
+        </p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
